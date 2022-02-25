dc7bedf8f5685bac86f9a9a0ba95f68dfed30d2d
arlobelshee <arlo@belshee.com>
2022-02-25 11:27:26 -0500

    r - (Ctrl+r, o) Move. Type new type to create.

---
+++ DemonstrateBuilders.Test/Spec/Arbitrary.cs
@@ -0,0 +1,4 @@
+internal static class Arbitrary
+{
+	public const string Email = "arbitrary.email@example.com";
+}
--- DemonstrateBuilders.Test/Spec/UserNotifications.cs
+++ DemonstrateBuilders.Test/Spec/UserNotifications.cs
@@ -8,18 +8,16 @@ namespace Tests.Spec
 {
 	public class UserNotifications
 	{
-		private const string Email = "arbitrary.email@example.com";
-
 		[Fact]
 		public void AskingForHelpShouldSendHowToEmail()
 		{
 			var loggedInUser =
-				new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, Email)}));
+				new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, Arbitrary.Email)}));
 			var testSubject = new SomePage(loggedInUser);
 			var result = testSubject.CreateHowToEmail();
 			result.Should().NotBeNull();
 			result.To.Should().HaveCount(1);
-			result.To.First().Should().BeEquivalentTo(new {Address = Email},
+			result.To.First().Should().BeEquivalentTo(new {Address = Arbitrary.Email},
 				options => options.ExcludingMissingMembers());
 			result.From.Should().Be("customer.support@example.com");
 		}
