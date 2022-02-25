f65ac453813b10b8bc9f5dfaaa9a8631057c6cb0
arlobelshee <arlo@belshee.com>
2022-02-25 11:58:56 -0500

    r - (Crtl+r,m) Extract method.

--- DemonstrateBuilders.Test/Spec/Make.cs
+++ DemonstrateBuilders.Test/Spec/Make.cs
@@ -5,6 +5,11 @@ internal static class Make
 {
 	public static ClaimsPrincipal Authentication()
 	{
+		return Build();
+	}
+
+	private static ClaimsPrincipal Build()
+	{
 		return new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, Arbitrary.Email)}));
 	}
 }
