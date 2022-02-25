3a8261a90754cc71181d26a5be82c1042070e7cd
arlobelshee <arlo@belshee.com>
2022-02-25 11:26:36 -0500

    r - (Ctrl+r,f) Introduce Field; select as constant.

--- DemonstrateBuilders.Test/Spec/UserNotifications.cs
+++ DemonstrateBuilders.Test/Spec/UserNotifications.cs
@@ -8,17 +8,18 @@ namespace Tests.Spec
 {
 	public class UserNotifications
 	{
+		private const string Email = "arbitrary.email@example.com";
+
 		[Fact]
 		public void AskingForHelpShouldSendHowToEmail()
 		{
-			var arbitraryEmailExampleCom = "arbitrary.email@example.com";
 			var loggedInUser =
-				new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, arbitraryEmailExampleCom)}));
+				new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, Email)}));
 			var testSubject = new SomePage(loggedInUser);
 			var result = testSubject.CreateHowToEmail();
 			result.Should().NotBeNull();
 			result.To.Should().HaveCount(1);
-			result.To.First().Should().BeEquivalentTo(new {Address = arbitraryEmailExampleCom},
+			result.To.First().Should().BeEquivalentTo(new {Address = Email},
 				options => options.ExcludingMissingMembers());
 			result.From.Should().Be("customer.support@example.com");
 		}
