6d01e4c3ec6155f3c1031ef9269009a3cf3bac86
arlobelshee <arlo@belshee.com>
2022-02-25 04:23:40 -0500

    a - Default project setup. A project and a test project that depends on it.

---
+++ DemonstrateBuilders.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System.Net.Mail;
+using Xunit;
+
+namespace DemonstrateBuilders.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            new Class1();
+        }
+    }
+}
