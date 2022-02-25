e86fb64c3fff47c5b1ad5145a5e4a793ccfddc16
arlobelshee <arlo@belshee.com>
2022-02-25 12:00:54 -0500

    r - (Alt+Enter) add `new AuthBuilder()` to call, then auto-fix 2x to create type and add it to the method.

--- DemonstrateBuilders.Test/Spec/Make.cs
+++ DemonstrateBuilders.Test/Spec/Make.cs
@@ -5,11 +5,15 @@ internal static class Make
 {
 	public static ClaimsPrincipal Authentication()
 	{
-		return Build();
+		return Build(new AuthBuilder());
 	}
 
-	private static ClaimsPrincipal Build()
+	private static ClaimsPrincipal Build(AuthBuilder authBuilder)
 	{
 		return new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {new(ClaimTypes.Email, Arbitrary.Email)}));
 	}
+}
+
+internal class AuthBuilder
+{
 }
