25cda5fbd3f3ee4c8a3556ca2086df189533ba9f
arlobelshee <arlo@belshee.com>
2022-03-17 16:51:04 -0400

    r - (Crtl+r, m) Extract Method.

--- DemonstrateBuilders.Test/Spec/UserNotifications.cs
+++ DemonstrateBuilders.Test/Spec/UserNotifications.cs
@@ -1,6 +1,7 @@
 using DemonstrateBuilders;
 using FluentAssertions;
 using System.Linq;
+using System.Net.Mail;
 using Tests.TestSupport;
 using Xunit;
 
@@ -19,13 +20,18 @@ namespace Tests.Spec
 
 			result.Should()
 				.NotBeNull();
+			BeTo(result);
+			result.From.Should()
+				.Be("customer.support@example.com");
+		}
+
+		private static void BeTo(MailMessage result)
+		{
 			result.To.Should()
 				.HaveCount(1);
 			result.To.First()
 				.Should()
 				.BeEquivalentTo(new {Address = Arbitrary.Email}, options => options.ExcludingMissingMembers());
-			result.From.Should()
-				.Be("customer.support@example.com");
 		}
 	}
 }
