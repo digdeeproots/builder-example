44ae3cf83e101794ef62811a59f79a5491a7feee
arlobelshee <arlo@belshee.com>
2022-03-19 15:02:03 -0400

    r - (Alt+Enter) Move class to its own file.

---
+++ DemonstrateBuilders.Test/TestSupport/MailMessageAssertions.cs
@@ -0,0 +1,33 @@
+using FluentAssertions;
+using FluentAssertions.Primitives;
+using System.Linq;
+using System.Net.Mail;
+using Tests.Spec;
+
+namespace Tests.TestSupport;
+
+internal class MailMessageAssertions : ReferenceTypeAssertions<MailMessage, MailMessageAssertions>
+{
+	public MailMessageAssertions(MailMessage subject) : base(subject) { }
+
+	protected override string Identifier => "mail message";
+
+	public AndConstraint<MailMessageAssertions> BeTo(params string[] recipients)
+	{
+		Subject.To.Should()
+			.BeEquivalentTo(recipients.Select(r => new {Address = r}), options => options.ExcludingMissingMembers());
+		return this.AllowingAnd();
+	}
+
+	public AndConstraint<MailMessageAssertions> BeFrom(string sender)
+	{
+		Subject.From.Should()
+			.Be(sender);
+		return this.AllowingAnd();
+	}
+
+	public AndConstraint<MailMessageAssertions> HaveContent(Mailing expected)
+	{
+		return this.AllowingAnd();
+	}
+}
--- DemonstrateBuilders.Test/TestSupport/TestExtensions.cs
+++ DemonstrateBuilders.Test/TestSupport/TestExtensions.cs
@@ -1,8 +1,5 @@
 using FluentAssertions;
-using FluentAssertions.Primitives;
-using System.Linq;
 using System.Net.Mail;
-using Tests.Spec;
 
 namespace Tests.TestSupport;
 
@@ -16,31 +13,5 @@ internal static class TestExtensions
 	public static AndConstraint<T> AllowingAnd<T>(this T assertions)
 	{
 		return new AndConstraint<T>(assertions);
-	}
-}
-
-internal class MailMessageAssertions : ReferenceTypeAssertions<MailMessage, MailMessageAssertions>
-{
-	public MailMessageAssertions(MailMessage subject) : base(subject) { }
-
-	protected override string Identifier => "mail message";
-
-	public AndConstraint<MailMessageAssertions> BeTo(params string[] recipients)
-	{
-		Subject.To.Should()
-			.BeEquivalentTo(recipients.Select(r => new {Address = r}), options => options.ExcludingMissingMembers());
-		return this.AllowingAnd();
-	}
-
-	public AndConstraint<MailMessageAssertions> BeFrom(string sender)
-	{
-		Subject.From.Should()
-			.Be(sender);
-		return this.AllowingAnd();
-	}
-
-	public AndConstraint<MailMessageAssertions> HaveContent(Mailing expected)
-	{
-		return this.AllowingAnd();
 	}
 }
