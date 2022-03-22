07d42bc2f2d982daaaa24b48616141a4022136c4
arlobelshee <arlo@belshee.com>
2022-03-22 16:59:10 -0400

    F - Hand edit, approve test result change. Improve mailing content. This is more like a real message.

--- DemonstrateBuilders/Mailings.cs
+++ DemonstrateBuilders/Mailings.cs
@@ -2,5 +2,9 @@ namespace DemonstrateBuilders;
 
 public static class Mailings
 {
-	public static Mailing HowTo() { return new Mailing("<p>This is the email <b>body.</b></p>", "How to email subject"); }
+	public static Mailing HowTo()
+	{
+		return new Mailing("<p>Hello, Sam.</p><p>Here's some help using <i>our awesome tool</i>.</p>",
+			"Help you requested on Wednesday");
+	}
 }
