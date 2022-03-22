6f0e021efc7ea2eb3656a96fba46955993acfdd5
arlobelshee <arlo@belshee.com>
2022-03-22 17:01:02 -0400

    r - (Alt+Enter, choose from list) Convert to string interpolation with interpolation argument.

--- DemonstrateBuilders/Mailings.cs
+++ DemonstrateBuilders/Mailings.cs
@@ -4,7 +4,7 @@ public static class Mailings
 {
 	public static Mailing HowTo()
 	{
-		return new Mailing("<p>Hello, Sam.</p><p>Here's some help using <i>our awesome tool</i>.</p>",
-			"Help you requested on Wednesday");
+		return new Mailing($"<p>Hello, {"Sam"}.</p><p>Here's some help using <i>our awesome tool</i>.</p>",
+			$"Help you requested on {"Wednesday"}");
 	}
 }
