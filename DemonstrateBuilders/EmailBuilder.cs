903bf1a28b4820cc9a0d9e62257547ae774afd74
arlobelshee <arlo@belshee.com>
2022-03-22 21:21:31 -0400

    r - (Alt+Enter) Autofix to convert field to local variable.

--- DemonstrateBuilders/EmailBuilder.cs
+++ DemonstrateBuilders/EmailBuilder.cs
@@ -5,13 +5,11 @@ namespace DemonstrateBuilders;
 
 public class EmailBuilder
 {
-	private readonly ClaimsPrincipal _recipient;
 	private readonly MailMessage _mailMessage;
 
 	public EmailBuilder(ClaimsPrincipal recipient)
 	{
-		_recipient = recipient;
-		_mailMessage = new MailMessage("customer.support@example.com", _recipient.ValueFor(ClaimTypes.Email));
+		_mailMessage = new MailMessage("customer.support@example.com", recipient.ValueFor(ClaimTypes.Email));
 	}
 
 	public MailMessage Build()
