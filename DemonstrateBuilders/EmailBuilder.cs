13071acbed90747cb7e063302ad3ef91c1aeb909
arlobelshee <arlo@belshee.com>
2022-03-22 21:17:27 -0400

    r - (Alt+Enter) Remove unused parameter.

--- DemonstrateBuilders/EmailBuilder.cs
+++ DemonstrateBuilders/EmailBuilder.cs
@@ -8,7 +8,7 @@ public class EmailBuilder
 	private readonly ClaimsPrincipal _recipient;
 	public EmailBuilder(ClaimsPrincipal recipient) { _recipient = recipient; }
 
-	public MailMessage Build(ClaimsPrincipal user)
+	public MailMessage Build()
 	{
 		return new MailMessage("customer.support@example.com", _recipient.ValueFor(ClaimTypes.Email));
 	}
