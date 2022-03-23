44010a5b1e1f8a6924c4a31698e3498b70d5fc7b
arlobelshee <arlo@belshee.com>
2022-03-22 21:20:58 -0400

    r - (Ctrl+r, f) introduce field. Choose to move init to c'tor.

--- DemonstrateBuilders/EmailBuilder.cs
+++ DemonstrateBuilders/EmailBuilder.cs
@@ -6,10 +6,16 @@ namespace DemonstrateBuilders;
 public class EmailBuilder
 {
 	private readonly ClaimsPrincipal _recipient;
-	public EmailBuilder(ClaimsPrincipal recipient) { _recipient = recipient; }
+	private readonly MailMessage _mailMessage;
+
+	public EmailBuilder(ClaimsPrincipal recipient)
+	{
+		_recipient = recipient;
+		_mailMessage = new MailMessage("customer.support@example.com", _recipient.ValueFor(ClaimTypes.Email));
+	}
 
 	public MailMessage Build()
 	{
-		return new MailMessage("customer.support@example.com", _recipient.ValueFor(ClaimTypes.Email));
+		return _mailMessage;
 	}
 }
