544837e02456c16538fb93032f48db76cccb678b
arlobelshee <arlo@belshee.com>
2022-03-22 21:17:06 -0400

    R - Delete duplicate assignment of same value to field before use. Hand-edit.

--- DemonstrateBuilders/EmailBuilder.cs
+++ DemonstrateBuilders/EmailBuilder.cs
@@ -5,12 +5,11 @@ namespace DemonstrateBuilders;
 
 public class EmailBuilder
 {
-	private ClaimsPrincipal _recipient;
+	private readonly ClaimsPrincipal _recipient;
 	public EmailBuilder(ClaimsPrincipal recipient) { _recipient = recipient; }
 
 	public MailMessage Build(ClaimsPrincipal user)
 	{
-		_recipient = user;
 		return new MailMessage("customer.support@example.com", _recipient.ValueFor(ClaimTypes.Email));
 	}
 }
